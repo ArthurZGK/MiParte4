167dfb230ea10136d86ef03f5c41fcc1aeafa6eb
Arthur <jarturopg_25@hotmail.com>
2020-08-25 22:40:51 -0500

    Anuncios Broadcast

--- app/src/main/java/com/example/miparte4/Servicio.java
+++ app/src/main/java/com/example/miparte4/Servicio.java
@@ -1,17 +1,25 @@
 package com.example.miparte4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class Servicio extends Activity {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+
         setContentView(R.layout.activity_servicio);
         Button arrancar = (Button) findViewById(R.id.boton_arrancar);
         arrancar.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +39,30 @@ public class Servicio extends Activity {
                         ServicioMusica.class));
             }
         });
+    }
+
+    public void superposicion(View view) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !android.provider.Settings.canDrawOverlays(this))
+        {
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            dialogo1.setTitle("Solicitud de permiso");
+            dialogo1.setMessage("Permita la superposicion de aplicaciones para poder activar su aplicación en primer plano en segundo plano");
+            dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    Intent permissionIntent = new Intent(android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivity(permissionIntent);
+                }
+            });
+            dialogo1.show();
+        }
+    }
+
+    public void custombroadcast(View view) {
+
+    }
+
+    public void miintentservice(View view) {
+        startActivity(new Intent(this, miIntentService.class));
     }
 }
